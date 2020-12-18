d8899c44aa153f1301fe87775f1893a57aeb7f73
Timo Volk <timo.volk@gmail.com>
2020-12-18 12:31:13 +0100

    moved .env into actual env for gh actions purposes

--- server/src/config/configuration.ts
+++ server/src/config/configuration.ts
@@ -1,8 +1,11 @@
-export default () => ({
-    use_static_assets: process.env.SERVE_STATIC || true,
-    db_user: process.env.MONGO_INITDB_ROOT_USERNAME,
-    db_pass:process.env.MONGO_INITDB_ROOT_PASSWORD,
-    db_uri: process.env.DB_HOST || "localhost",    
-    db_port: process.env.DB_PORT || 27017,
-    db_name: process.env.DB_NAME || "nest"
-}); 
+export default () =>{
+    //console.log(process.env)
+    return {
+        use_static_assets: process.env.SERVE_STATIC || true,
+        db_user: process.env.MONGO_INITDB_ROOT_USERNAME,
+        db_pass:process.env.MONGO_INITDB_ROOT_PASSWORD,
+        db_uri: process.env.DB_HOST || "localhost",    
+        db_port: process.env.DB_PORT || 27017,
+        db_name: process.env.DB_NAME || "nest"
+    }
+};
