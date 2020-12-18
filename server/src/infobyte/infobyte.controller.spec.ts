1434a53a5b76ce0e854e7a465ec7ab0b08d7b37a
vovinhd <timo.volk@gmail.com>
2020-12-18 13:51:44 +0100

    fixed mongoose unittest testrunner

--- server/src/infobyte/infobyte.controller.spec.ts
+++ server/src/infobyte/infobyte.controller.spec.ts
@@ -1,11 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { InfobyteController } from './infobyte.controller';
+import * as mongoDbCfg from '../../globalConfig.json'
+import { MongooseModule } from '@nestjs/mongoose';
+import { InfoByte } from './schemas/infobyte.schema';
+import { InfoBitSchema } from './schemas/infobit.schema';
+import { InfobyteService } from './infobyte.service';
+
 
 describe('InfobyteController', () => {
   let controller: InfobyteController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [MongooseModule.forRoot(mongoDbCfg.mongoUri), MongooseModule.forFeature([{ name: InfoByte.name, schema: InfoBitSchema}])],
+      providers: [InfobyteService],
       controllers: [InfobyteController],
     }).compile();
 
--- server/src/tree-template/tree-template.controller.spec.ts
+++ server/src/tree-template/tree-template.controller.spec.ts
@@ -1,11 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TreeTemplateController } from './tree-template.controller';
+import { TreeTemplateService } from './tree-template.service';
+import { TreeTemplate, TreeTemplateSchema, TreeTemplateDocument } from './schemas/treeTemplate.schema'
+import { MongooseModule } from '@nestjs/mongoose';
+import * as mongoDbCfg from '../../globalConfig.json'
 
 describe('TreeTemplateController', () => {
   let controller: TreeTemplateController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [MongooseModule.forRoot(mongoDbCfg.mongoUri), MongooseModule.forFeature([{ name: TreeTemplate.name, schema: TreeTemplateSchema}])],
+      providers: [TreeTemplateService],
       controllers: [TreeTemplateController],
     }).compile();
 
