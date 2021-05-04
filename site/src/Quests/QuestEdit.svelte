2a799c47f7a7daf1cfbb8a61f4bbd487c5265dfa
Timo Volk <timo.volk@gmail.com>
2021-05-04 20:52:11 +0200

    aaaa

--- site/src/Quests/QuestEdit.svelte
+++ site/src/Quests/QuestEdit.svelte
@@ -2,10 +2,14 @@
     import { onMount } from "svelte";
 
     import { Button, Col, Input } from "sveltestrap";
+    import AspectAsyncCombobox from "../Infobytes/atoms/AspectAsyncCombobox.svelte";
+import { fetchAspects } from "../Infobytes/InfobyteService";
     import { QuestDto, QuestLocalization } from "./QuestTypes";
     export let questModifiedCallback;
 
     export let quest: QuestDto;
+
+    let aspectsPromise: Promise<any[]> = fetchAspects(quest.region, quest.questLocalization[0]?.language);
 
     const save = () => {};
     const addLocale = () => {
@@ -100,19 +104,17 @@
             type="string"
             bind:value={quest.linkToAfter}
         />
+        <AspectAsyncCombobox
+            inputId={"alertTrackedAspectInput"}
+            name={"Aspekt Tracking abfragen?"}
+            bind:value={quest.alertTrackedAspect}
+            optionsPromise={aspectsPromise}
+        />
     </div>
 
-    <label for="alertTrackedAspectInput">Aspekt Tracking abfragen?</label>
-    <Input
-        id="alertTrackedAspectInput"
-        name="alertTrackedAspectInput"
-        type="select"
-        bind:value={quest.alertTrackedAspect}
-    >
-        <option value={null}> -- Kein Aspekt -- </option>
-        <option>DE</option>
-    </Input>
+
     
+
 
     <Col xs="6">
         <Button on:click={save}>Speichern</Button>
