a91e31d5456ab3cd333ff1857c306995c484e717
vovinhd <timo.volk@gmail.com>
2022-05-04 22:36:13 +0200

    removed that whole FieldSetExtractor idea

---
+++ frontend/src/lib/components/Editor/content-editor-actions.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { Content } from "./editor-types";
+
+    export let data: Content;
+</script>
+
+<div>
+    <nav class="grid grid-flow-col">
+        <h1>Content Editor</h1>
+        <label> Veröffentlichen <input type="checkbox" bind:checked={data.published}/></label>
+        <button>Speichern</button>
+    </nav>
+</div>
